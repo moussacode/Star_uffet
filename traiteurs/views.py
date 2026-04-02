d39684fe12ea4ec369aba75d781fef0c389c27f9
moussacode <samouh269@gmail.com>
2026-04-02 09:47:23 +0000

    save current work

--- traiteurs/views.py
+++ traiteurs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import Http404
 from . import models
 # Create your views here.
@@ -7,8 +8,14 @@ from .form import DevenirTraiteurForm
 
 def traiteurs(request):
 
-    traiteurs = models.Traiteur.objects.all()
+    traiteurs_list = models.Traiteur.objects.all()
     specialites = models.Specialite.objects.all()
+
+    pagination = Paginator(traiteurs_list,2)
+
+    page_number = request.GET.get('page')
+    traiteurs = pagination.get_page(page_number) 
+
     contexte ={
         'traiteurs': traiteurs,
         'specialites':specialites 
